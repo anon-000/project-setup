1fe1b2fc9f232251a28bfb61ad7794bbb1c3d3cb
aurosmruti <000computricks@gmail.com>
2020-10-25 01:46:54 +0530

    init

--- project_setup.sh
+++ project_setup.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
 
+# Created by Auro on 25th October at 1:17AM
+# Dussehra ta chalichy au mu basiki aeya karuchy .. kan kariba au .. 
+# vabideichy taw karibaku padiba au ... au smt re a bala corona ... 
+# pura flop kridela dussehra maja ...
+# chada au ... Lets hope for the best ... Vagawan nku etiki pratharna...
+# Please ae project ta atleast complete hbaku diyaaaaaaaaaaaaaaaaaaaaaaaaaa....
+# Toh Chaliyeeeee Shuru Karte hein ...  
+
+
+#variables
+REPO_NAME=''
+PACKAGE_NAME=''
+APP_NAME=''
+REPO_URL=''
+
+# gets boilerplate code and process it
+get_code_and_process(){
+    git clone https://gitlab.com/e-commerce-basic/e-commerce-flutter.git
+}
+
 
 echo Hellow World
 
